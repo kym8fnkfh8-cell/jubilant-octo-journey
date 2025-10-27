80b883a21bf6d924c4e7e0c3c16bd1d6fade4837
kym8fnkfh8-cell <kym8fnkfh8@privaterelay.appleid.com>
2025-10-27 15:06:06 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'osrs-planner-v4-3d'; // cache bump
+const CACHE_NAME = 'osrs-planner-v4-3c'; // cache bump
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./favicon.png','./animated-icon.gif'];
 self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE_NAME?null:caches.delete(k))))); self.clients.claim();});
