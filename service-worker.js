073709a7031862a0764d406ccbf3c2450b1e9f86
kym8fnkfh8-cell <kym8fnkfh8@privaterelay.appleid.com>
2025-10-27 15:09:20 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,6 @@
-const CACHE_NAME = 'osrs-planner-v4-3c'; // cache bump
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./favicon.png','./animated-icon.gif'];
+
+const CACHE_NAME = 'osrs-planner-v4-1e';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE_NAME?null:caches.delete(k))))); self.clients.claim();});
-self.addEventListener('fetch',(e)=>{
-  const req=e.request;
-  e.respondWith(
-    caches.match(req).then(res=>res||fetch(req).then(net=>{
-      if(req.method==='GET' && new URL(req.url).origin===location.origin){
-        const clone=net.clone();
-        caches.open(CACHE_NAME).then(c=>c.put(req, clone));
-      }
-      return net;
-    }).catch(()=>caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch',(e)=>{const req=e.request; e.respondWith(caches.match(req).then(res=>res||fetch(req).then(net=>{ if(req.method==='GET' && new URL(req.url).origin===location.origin){ const clone=net.clone(); caches.open(CACHE_NAME).then(c=>c.put(req, clone)); } return net; }).catch(()=>caches.match('./index.html'))));});
